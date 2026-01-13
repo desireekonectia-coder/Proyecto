6b98200823e754a408fe4f114a4cb83f7eb954e4
Desirée <desireekonectia@gmail.com>
2026-01-13 09:31:06 +0100

    Cambios

--- Back/Scrips/app.js
+++
@@ -1,6 +0,0 @@
-// Este mensaje aparecerá en la consola del navegador (F12)
-console.log("El archivo JavaScript se ha cargado correctamente desde la carpeta Back.");
-
-function saludar() {
-    alert("¡Hola desde el Script!");
-}
---
+++ Back/Scripts/app.js
@@ -0,0 +1,6 @@
+// Este mensaje saldrá en la consola al cargar la página
+console.log("Archivo cargado con éxito");
+
+function saludar() {
+    alert("¡Hola! El enlace funciona perfectamente.");
+}
